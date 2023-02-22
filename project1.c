1c1a49d8b954f905c32d40e8ac4666d85fe01cdd
Daniel Yowell <danielyowell@ou.edu>
2023-02-21 23:32:30 -0600

    basic fork program

--- project1.c
+++ project1.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <time.h>
+#include <unistd.h>
 
 int main(int argc, char* argv[]) {
+  fork();
+  printf("Hello world");
   return 0;
 }
