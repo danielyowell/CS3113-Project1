2487591b31fd7ad20928b1a1547cebc250781343
Daniel Yowell <danielyowell@ou.edu>
2023-02-21 23:54:31 -0600

    verified fork() and wait()

--- project1.c
+++ project1.c
@@ -4,7 +4,23 @@
 #include <unistd.h>
 
 int main(int argc, char* argv[]) {
-  fork();
-  printf("Hello world\n");
+  int id = fork();
+  int n;
+  if(id == 0) {
+    n = 1; 
+  }
+  else {
+    n = 6;
+  }
+  if(id != 0) {
+    wait();
+  }
+  for(int i = n; i < n + 5; i++) {
+    printf("%d ", i);
+    fflush(stdout);
+  }
+  if(id != 0) {
+    printf("\n");
+  }
   return 0;
 }
