bbe3d1abd876f56cc11eb48e757ba080490eec42
Daniel Yowell <danielyowell@ou.edu>
2023-02-23 22:32:00 -0600

    working version?

--- project1.c
+++ project1.c
@@ -1,32 +1,112 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+// PROOF OF CONCEPT: SHARED MEMORY (the hard way)
 #include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
 
-/* total created prior to fork */
-int total = 0;
+/* key number */
+#define SHMKEY ((key_t) 1497)
 
-int main(int argc, char* argv[]) {
+/* defines struct shared_mem with int variable "value" */
+typedef struct
+{
+    int value;
+} shared_mem;
 
-  /* create child */
-  int id = fork();
+/* create shared memory (pointer) */
+shared_mem *total;
 
-  /* parent waits for child to complete */
-  if(id != 0) {
-    wait();
-  }
+void process1() {
+    while(total->value < 100000) {
+        total->value = total->value + 1;
+    }
+    printf("From Process 1: counter = %d.\n", total->value);
+    printf("Child with ID: %d has just exited.\n", getpid());
+    return;
+}
+void process2() {
+    while(total->value < 200000) {
+        total->value = total->value + 1;
+    }
+    printf("From Process 2: counter = %d.\n", total->value);
+    printf("Child with ID: %d has just exited.\n", getpid());
+    return;
+}
+void process3() {
+    while(total->value < 300000) {
+        total->value = total->value + 1;
+    }
+    printf("From Process 3: counter = %d.\n", total->value);
+    printf("Child with ID: %d has just exited.\n", getpid());
+    return;
+}
+void process4() {
+    while(total->value < 500000) {
+        total->value = total->value + 1;
+    }
+    printf("From Process 4: counter = %d.\n", total->value);
+    printf("Child with ID: %d has just exited.\n", getpid());
+    return;
+}
 
-  /* child code */
-  if(id == 0) {
-    total = 100;
-    printf("child: total is %d\n", total);
-  }
+int main(void)
+{
+    int shmid = shmget (SHMKEY, sizeof(int), IPC_CREAT | 0666);
+    
+    /* main function address */
+    char *shmadd;
+    shmadd = (char *) 0;
+    
+    total = (shared_mem*) shmat (shmid, shmadd, 0);
+    
+    total->value = 0;
+    /* printf("current value: %d\n", total->value); */
 
-  /* run by parent process */
-  if(id != 0) {
-    printf("parent: total is %d\n", total);
-    printf("End of simulation\n");
-  }
-
-  return 0;
+    pid_t id;
+    
+    id = fork();
+    /* child process */
+    if(id == 0) {
+        process1();
+    }
+    if(id != 0) {
+        wait(NULL);
+        id = fork();
+        if(id == 0) {
+            process2();
+        }
+        if(id != 0) {
+            wait(NULL);
+            id = fork();
+            if(id == 0) {
+                process3();
+            }
+            if(id != 0) {
+                wait(NULL);
+                id = fork();
+                if(id == 0) {
+                    process4();
+                }
+                /* this is the parent process */
+                if(id != 0) {
+                    wait(NULL);
+                    /* what is the difference between detaching and deleting shared memory? */
+                            if (shmdt(total) == -1) {
+                                perror ("shmdt");
+                                exit (-1);
+                            }   
+                            shmctl(shmid, IPC_RMID, NULL); 
+                    printf("\nEnd of simulation.\n");
+                }
+            }
+        }
+    }
 }
